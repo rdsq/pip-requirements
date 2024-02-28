17d29408f5f03d753e8c78a8bcd085563f1e77f6
rdsq <62446963+rdsq@users.noreply.github.com>
2024-02-28 18:26:05 +0200

    1.0.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,22 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.install', (textEditor, edit) => {
+	function fromTemplate(textEditor: any, text: string) {
 		if (textEditor) {
 			const terminal = vscode.window.createTerminal();
-			terminal.sendText(`python -m pip install -r "${textEditor.fsPath}"`);
+			terminal.sendText(text);
 			terminal.show();
 		} else {
-			vscode.window.showInformationMessage('Cannot run this command. Try to use it by clicking on the icon in pip requirements');
+			vscode.window.showErrorMessage('Cannot run this command. Try to use it by clicking on the icon in pip requirements');
 		}
+	}
+	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.install', (textEditor, edit) => {
+		fromTemplate(textEditor, `python -m pip install -r "${textEditor.fsPath}"`);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.freeze', (textEditor, edit) => {}));
+	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.freeze', (textEditor, edit) => {
+		fromTemplate(textEditor, `python -m pip freeze > "${textEditor.fsPath}"`);
+	}));
 }
 
 export function deactivate() {}
