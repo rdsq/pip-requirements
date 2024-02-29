b7937428de647208991216745d76df970b15974e
rdsq <62446963+rdsq@users.noreply.github.com>
2024-02-29 18:17:45 +0200

    Use the extension from the command pallete

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,32 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	function fromTemplate(textEditor: any, text: string) {
-		if (textEditor) {
+	function fromTemplate(textEditor: any, command: string) {
+		function runCommand(path: string) {
 			const terminal = vscode.window.createTerminal();
-			terminal.sendText(text);
+			terminal.sendText(`python -m pip ${command} "${path}"`);
 			terminal.show();
+		}
+		if (textEditor) {
+			runCommand(textEditor.fsPath);
 		} else {
-			vscode.window.showErrorMessage('Cannot run this command. Try to use it by clicking on the icon in pip requirements');
+			vscode.window.showInputBox({
+				title: 'Path to the requirements.txt file',
+				value: 'requirements.txt',
+				placeHolder: 'requirements.txt'
+			}).then(path => {
+				if (path !== undefined) {
+					runCommand(path);
+				}
+			});
 		}
 	}
 	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.install', (textEditor, edit) => {
-		fromTemplate(textEditor, `python -m pip install -r "${textEditor.fsPath}"`);
+		fromTemplate(textEditor, 'install -r');
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('pip-requirements.freeze', (textEditor, edit) => {
-		fromTemplate(textEditor, `python -m pip freeze > "${textEditor.fsPath}"`);
+		fromTemplate(textEditor, 'freeze >');
 	}));
 }
 
